afa1d652bc4d7a8ed47f35ff2e95ecc3a1863cb8
fsamoylov <fsamoylov@abaqs.ru>
2014-03-18 15:00:04 +0400

    Added max date, dates color

--- afl-mdp.js
+++ afl-mdp.js
@@ -4,6 +4,11 @@ jQuery(function(){
     var calendar = $('#cal');
     var inputs = $("#date1, #date2, #date3, #date4");
     
+    var input_dates = [];
+    var dates_time_array = [];
+    var dates_min = 0;
+    var dates_max = 0;
+    
     function isValid(input){
         var tempDate = calendar.datepicker('getDate');
         
@@ -37,6 +42,11 @@ jQuery(function(){
         inputs.not('.invalid').each(function(i,e){
             e.value = dates[i] || '';
         });
+        input_dates = jQuery.map(inputs.not('.invalid'),
+                                 function(i,e){return $.datepicker.parseDate(dateFormat, i.value)});
+        dates_time_array = jQuery.map(input_dates, function(i){ return i.getTime()});
+        dates_min = Math.min.apply(null, dates_time_array);
+        dates_max = Math.max.apply(null, dates_time_array);
         
         calendar.datepicker('refresh');
     });
@@ -44,26 +54,31 @@ jQuery(function(){
     inputs.focusout(function(){
         lastFocused = this;
     });
+    
     calendar.datepicker(
         {
             onSelect:
                 function(date){
                     $(lastFocused).val(date).focus();
-                    inputs.filter('[value=""]').first().focus()
+                    inputs.filter('[value=""]').first().focus();
                     inputs.change();
-                    
                 },
+            
             dateFormat: dateFormat,
             minDate: 0,
+            maxDate: 330,
             numberOfMonths: 2,
             beforeShowDay: function(date){
-                var input_dates = jQuery.map(inputs, function(i,e){return i.value})
-                date = $.datepicker.formatDate(dateFormat, date);
-                i = input_dates.indexOf(date)
+                var datetime = date.getTime();
+                i = dates_time_array.indexOf(datetime);
                 if(i != -1){
-                    return [true, 'sel_date date_'+(i+1)]
+                    return [true, 'sel_date date_'+(i+1)];
+                } else if(dates_time_array.length > 1
+                          && datetime > dates_min
+                          && datetime < dates_max){
+                    return [true, 'sel_date'];
                 } else {
-                    return [true, '']
+                    return [true, ''];
                 }
             }
         }
